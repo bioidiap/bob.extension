13304bebfd440ea9aa6febd8ee736db406d6623e
Manuel Guenther <manuel.guenther@idiap.ch>
2015-03-23 16:35:14 +0100

    Improved dependency_graph script

--- bob/extension/scripts/dependency_graph.py
+++ bob/extension/scripts/dependency_graph.py
@@ -1,7 +1,13 @@
 #!../bin/python
 
+"""
+This script will generate a dependency graph of the given bob package(s) using the external tool ``dot``.
+Packages can be either specified as a list of ``--packages`` or read from (several) ``--package-files``.
+The latter option is mostly useful to generate a dot graph for all Bob packages.
 
-#!/usr/bin/env python
+The output is written to the given ``--output-file``, writing either the specified intermediate ``--dot-file``, or a temporary file.
+When the ``--plot-external-dependencies`` is selected, also external (Python-)dependencies will be plotted as well, in red ellipses.
+"""
 
 from __future__ import print_function
 import subprocess
@@ -11,15 +17,6 @@ import tempfile, os
 import argparse
 
 def main(command_line_options = None):
-  """
-  This script will generate a dependency graph of the given bob package(s) using the external tool ``dot``.
-  Packages can be either specified as a list of ``--packages`` or read from (several) ``--package-files``.
-  The latter option is mostly useful to generate a dot graph for all Bob packages.
-  
-  The output is written to the given ``--output-file``, writing either the specified intermediate ``--dot-file``, or a temporary file.
-  When the ``--plot-external-dependencies`` is selected, also external (Python-)dependencies will be plotted as well, in red ellipses.
-  """
-
   parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
 
   parser.add_argument("--packages", '-p', nargs = '+', default = [], help = "Which packages do you want to have dependencies for?")
@@ -28,6 +25,7 @@ def main(command_line_options = None):
   parser.add_argument("--output-file", "-w", default="dependencies.png", help = "Specify the (.png) file to write.")
   parser.add_argument("--limit-packages", '-l', nargs = '+', default = ['bob', 'facereclib', 'antispoofing'], help = "Limit packages read from --package-files to the given namespaces")
   parser.add_argument("--plot-external-dependencies", '-X', action='store_true', help = "Include external dependencies into the plot?")
+  parser.add_argument("--rank-base-tools-same", '-R', action = 'store_true', help = "Set the rank of packages bob.extension, bob.core and bob.blitz at the same size")
   parser.add_argument("--vertical", '-V', action = 'store_true', help = "Display the dot graph in vertical direction")
   parser.add_argument("--verbose", '-v', action = 'store_true', help = "Print more information")
 
@@ -41,7 +39,7 @@ def main(command_line_options = None):
     for line in open(package_file):
       splits = line.rstrip().split()
       packages.extend([p for p in splits if p not in packages and p.startswith(args.limit_packages)])
-      
+
   # generate dependencies
   dependencies = {}
   cpp_dependencies = {}
@@ -56,18 +54,18 @@ def main(command_line_options = None):
       deps = pkg_resources.require(p)
       dependencies[p] = [d.key for d in deps[1:]]
       if args.plot_external_dependencies and p.startswith(args.limit_packages):
-        # also load the C++ dependencies, stored in the .version package 
+        # also load the C++ dependencies, stored in the .version package
         import importlib
         lib = importlib.import_module(p)
         try:
           cpp_dependencies[p] = [dep for dep in lib.version.externals.keys() if not dep.startswith(args.limit_packages)]
         except AttributeError:
           cpp_dependencies[p] = []
-          
+
       for d in dependencies[p]:
         has_parents.add(d)
         _add_recursive(d)
-    
+
   for package in packages:
     _add_recursive(package)
 
@@ -81,7 +79,7 @@ def main(command_line_options = None):
   bob = set(package for package in pruned_dependencies if package.startswith(args.limit_packages))
   non_bob = set(dep for package in bob for dep in pruned_dependencies[package] if not dep.startswith(args.limit_packages))
   final = set(package for package in bob if package not in has_parents)
-  
+
   # also prune the C++ dependencies
   if args.plot_external_dependencies:
     pruned_cpp_dependencies = {}
@@ -90,7 +88,7 @@ def main(command_line_options = None):
       pruned_cpp_dependencies[package] = [dep for dep in cpp_dependencies[package] if dep not in indirect_dependencies]
 
     cpp = set(dep for package in bob for dep in pruned_cpp_dependencies[package])
-  
+
 
   # function to return a name for the package that can serve as a dot variable
   def _n(p):
@@ -125,22 +123,26 @@ def main(command_line_options = None):
       if args.plot_external_dependencies:
         for dep in pruned_cpp_dependencies[package]:
           f.write('\t%s -> %s [color=red,style=dashed];\n' % (_n(package), _n(dep)))
-        
+
+    # rank base tools at the same level
+    if args.rank_base_tools_same:
+      f.write('\t{rank=same; bob_extension bob_core bob_blitz}\n')
+
     f.write("}\n")
-  
+
   if args.verbose and args.dot_file is not None:
     print("Wrote dot file %s" % dot_file)
-    
+
   # call dot
   call = ["dot",  '-y', '-o', args.output_file, '-Tpng:cairo:gd', dot_file]
   if args.verbose:
     call[1:1] = ['-v']
     print("Calling dot: '%s'" % " ".join(call))
   subprocess.call(call)
-  
+
   if args.verbose:
     print("\nWrote file %s" % args.output_file)
-  
+
   # clean-up
   if args.dot_file is None:
     os.remove(dot_file)
