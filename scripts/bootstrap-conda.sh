03a71177a22878dabd41f0f277840ff8cfc2cf1b
Andre Anjos <andre.anjos@idiap.ch>
2016-08-08 17:58:14 +0200

    [scripts] Bootstrapping with conda (fixed)

--- scripts/bootstrap-conda.sh
+++ scripts/bootstrap-conda.sh
@@ -11,8 +11,12 @@ PYTHON_VER=$2
 PREFIX=`pwd`/build-env
 
 # Clones the conda dev environment to use
-echo ${CONDA_FOLDER}/bin/conda create --prefix ${PREFIX} --clone `cat ${CONDA_FOLDER}/envs/latest-devel-${PYTHON_VER}.txt` --offline --yes
-${CONDA_FOLDER}/bin/conda create --prefix ${PREFIX} --clone `cat ${CONDA_FOLDER}/envs/latest-devel-${PYTHON_VER}.txt` --offline --yes
+if [ ! -d ${PREFIX} ]; then
+  echo ${CONDA_FOLDER}/bin/conda create --prefix ${PREFIX} --clone `cat ${CONDA_FOLDER}/envs/latest-devel-${PYTHON_VER}.txt` --offline --yes
+  ${CONDA_FOLDER}/bin/conda create --prefix ${PREFIX} --clone `cat ${CONDA_FOLDER}/envs/latest-devel-${PYTHON_VER}.txt` --offline --yes
+else
+  echo "prefix directory `${PREFIX}' exists. not re-bootstrapping..."
+fi
 
 # Install this package's dependencies
 echo source ${CONDA_FOLDER}/bin/activate `pwd`/build-env
@@ -22,10 +26,14 @@ if [ -e requirements.txt ]; then
   echo "Using pip:" `which pip`
   echo pip install --find-links 'https://www.idiap.ch/software/bob/wheels/gitlab/' --use-wheel --no-index --trusted-host 'www.idiap.ch' --pre --requirement requirements.txt
   pip install --find-links 'https://www.idiap.ch/software/bob/wheels/gitlab/' --use-wheel --no-index --trusted-host 'www.idiap.ch' --pre --requirement requirements.txt
+else
+  echo "no requirements.txt file found, skipping `pip install'..."
 fi
 
 if [ -e bootstrap-buildout.py ]; then
   echo "Using python:" `which python`
   echo "python" bootstrap-buildout.py
   python bootstrap-buildout.py
+else
+  echo "no bootstrap-buildout.py file found, skipping `buildout bootstrap'..."
 fi
