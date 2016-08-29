f411b5f0c99b953761e1fac515fc15577e81b5fb
Amir Mohammadi <183.amir@gmail.com>
2016-08-29 22:38:54 +0200

    initial commit for ci with conda

---
+++ bootstrap-conda.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+# Wed 17 Aug 2016 13:50:09 CEST
+set -ex
+
+if [ "${#}" != 2 ]; then
+  echo "usage: `basename $0` <prefix> <python-version>"
+  echo "example: `basename $0` /opt/conda 2.7"
+  exit 1
+fi
+
+BASEDIR=$1
+PYTHON_VERSION=$2
+CONDA=${BASEDIR}/bin/conda
+MINICONDA=${HOME}/Downloads/miniconda.sh
+
+if [ "$(uname)" == "Darwin" ]; then
+  ARCH="MacOSX-x86_64"
+else
+  ARCH="Linux-x86_64"
+fi
+
+if [[ "$PYTHON_VERSION" == "2.7" ]]; then
+  PYK="2"
+else
+  PYK="3"
+fi
+
+# Create root environment and add basic channels for conda
+if [ ! -x ${CONDA} ]; then
+  if [ ! -x ${MINICONDA} ]; then
+    mkdir -pv `dirname ${MINICONDA}`
+    echo "[>>] Downloading `basename ${MINICONDA}` -> ${MINICONDA}..."
+    curl --progress-bar https://repo.continuum.io/miniconda/Miniconda${PYK}-latest-${ARCH}.sh --output ${MINICONDA}
+    chmod 755 ${MINICONDA}
+  fi
+
+  echo "[>>] Creating root environment and setting basic options..."
+  bash ${MINICONDA} -b -p ${BASEDIR}
+  ${CONDA} config --set show_channel_urls True
+  ${CONDA} install --yes -n root conda-build sphinx
+  ${CONDA} config --add channels conda-forge
+  ${CONDA} config --add channels ${TEST_CHANNEL}
+fi
+
+echo "[>>] Updating conda in the root environment..."
+${CONDA} update --yes -n root conda
+${CONDA} info
+
+# Function for running command and echoing results
+run_cmd() {
+  echo "[(`date +%c`)>>] Running \"${@}\"..."
+  ${@}
+  if [ "$?" != "0" ]; then
+    echo "[(`date +%c`)!!] Command Failed \"${@}\""
+    exit 1
+  else
+    echo "[(`date +%c`)<<] Finished comand \"${@}\""
+  fi
+}
