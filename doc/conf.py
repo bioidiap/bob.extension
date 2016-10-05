73419b45faa8d027ed5326a2112455c6855684b7
Amir Mohammadi <183.amir@gmail.com>
2016-10-05 10:57:46 +0200

    [Automatic] update links and the ci mostly

--- doc/conf.py
+++ doc/conf.py
@@ -131,7 +131,7 @@ pygments_style = 'sphinx'
 
 # Some variables which are useful for generated material
 project_variable = project.replace('.', '_')
-short_description = u''
+short_description = u'Building of Python/C++ extensions for Bob'
 owner = [u'Idiap Research Institute']
 
 
