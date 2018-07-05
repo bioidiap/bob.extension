b8a5a810b3a56e4823340a3b3b7e07bf81c2fcae
Theophile GENTILHOMME <tgentilhomme@jurasix08.idiap.ch>
2018-07-05 15:21:11 +0200

    Add test file for config file generation

---
+++ bob/extension/data/test_dump_config2.py
@@ -0,0 +1,52 @@
+'''Blablabla bli blo
+
+
+Parameters
+----------
+xxx : :any:`list`
+    blabla blablo
+yyy : callable
+    bli bla blo bla bla bla
+
+[CONFIG]...           BLA BLA BLA BLA'''
+
+
+## bla bla bla.
+## Option: --database, -d [default: None]
+# database = None
+
+
+
+
+## bli bli bli.
+## Option: --annotator, -a [default: None]
+# annotator = None
+
+
+
+
+## blo blo blo.
+## Option: --output-dir, -o [default: None]
+# output_dir = None
+
+
+
+
+## lalalalalala.
+## Option: --force, -f [default: False]
+# force = False
+
+
+
+
+## lililili.
+## Option: --array [default: 1]
+# array = 1
+
+
+
+
+## lklklklk.
+## Option: --database-directories-file [default: ~/databases.txt]
+# database_directories_file = ~/databases.txt
+
